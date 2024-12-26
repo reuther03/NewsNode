dd883107b819e75894cca0e0fc21a3bd9d8bdb68
michaldrozd <michaldrozd03@gmail.com>
2024-12-26 03:53:47 +0100

    Added PostDto

---
+++ src/Modules/Socials/NewsNode.Modules.Socials.Application/Features/Queries/Dtos/PostDto.cs
@@ -0,0 +1,32 @@
+﻿using NewsNode.Modules.Socials.Domain.Post;
+
+namespace NewsNode.Modules.Socials.Application.Features.Queries.Dtos;
+
+public class PostDto
+{
+    public Guid Id { get; init; }
+    public string Content { get; init; } = null!;
+    public DateTime PostedAt { get; init; }
+    public Guid CreatedBy { get; init; }
+    public int Likes { get; init; }
+    public int Bookmarks { get; init; }
+    public int Reposts { get; init; }
+    public List<string> Hashtags { get; init; } = [];
+    public int Comments { get; init; }
+
+    public static PostDto AsDto(Post post)
+    {
+        return new PostDto
+        {
+            Id = post.Id,
+            Content = post.Content,
+            PostedAt = post.PostedAt,
+            CreatedBy = post.CreatedBy,
+            Likes = post.Likes,
+            Bookmarks = post.Bookmarks,
+            Reposts = post.Reposts,
+            Hashtags = post.Hashtags.Select(x => x.Value).ToList(),
+            Comments = post.Comments.Count
+        };
+    }
+}
