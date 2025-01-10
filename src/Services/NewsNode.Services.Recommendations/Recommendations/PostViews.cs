c15d9409d8867cd01df4998714169b5ffa90daeb
michaldrozd <michaldrozd03@gmail.com>
2025-01-10 18:48:57 +0100

    Added PostViews entity

---
+++ src/Services/NewsNode.Services.Recommendations/Recommendations/PostViews.cs
@@ -0,0 +1,10 @@
+﻿using NewsNode.Shared.Abstractions.Kernel.Primitives;
+using NewsNode.Shared.Abstractions.Kernel.ValueObjects.Ids;
+
+namespace NewsNode.Services.Recommendations.Recommendations;
+
+public class PostViews : Entity<Guid>
+{
+    public PostId PostId { get; private set; }
+    public int Views { get; private set; }
+}
