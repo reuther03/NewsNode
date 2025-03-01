8d50d9d5c29e35475b0d1b39e84c997d1eb7a7db
michaldrozd <michaldrozd03@gmail.com>
2025-03-01 01:37:35 +0100

    Added img to comment

--- src/Modules/Socials/NewsNode.Modules.Socials.Infrastructure/Database/Migrations/20250301003436_AddedCommentEntity.Designer.cs
+++ src/Modules/Socials/NewsNode.Modules.Socials.Infrastructure/Database/Migrations/20250301003436_AddedCommentEntity.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NewsNode.Modules.Socials.Infrastructure.Database;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace NewsNode.Modules.Socials.Infrastructure.Database.Migrations
 {
     [DbContext(typeof(SocialsDbContext))]
-    partial class SocialsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250301003436_AddedCommentEntity")]
+    partial class AddedCommentEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Modules/Socials/NewsNode.Modules.Socials.Infrastructure/Database/Migrations/20250301003436_AddedCommentEntity.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NewsNode.Modules.Socials.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCommentEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PostImg",
+                schema: "socials");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ContentImgId",
+                schema: "socials",
+                table: "Comments",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ContentImg",
+                schema: "socials",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FileUrl = table.Column<string>(type: "text", nullable: false),
+                    FileName = table.Column<string>(type: "text", nullable: false),
+                    PostId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContentImg", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ContentImg_Posts_PostId",
+                        column: x => x.PostId,
+                        principalSchema: "socials",
+                        principalTable: "Posts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_ContentImgId",
+                schema: "socials",
+                table: "Comments",
+                column: "ContentImgId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContentImg_PostId",
+                schema: "socials",
+                table: "ContentImg",
+                column: "PostId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_ContentImg_ContentImgId",
+                schema: "socials",
+                table: "Comments",
+                column: "ContentImgId",
+                principalSchema: "socials",
+                principalTable: "ContentImg",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_ContentImg_ContentImgId",
+                schema: "socials",
+                table: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "ContentImg",
+                schema: "socials");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_ContentImgId",
+                schema: "socials",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "ContentImgId",
+                schema: "socials",
+                table: "Comments");
+
+            migrationBuilder.CreateTable(
+                name: "PostImg",
+                schema: "socials",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FileName = table.Column<string>(type: "text", nullable: false),
+                    FileUrl = table.Column<string>(type: "text", nullable: false),
+                    PostId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostImg", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PostImg_Posts_PostId",
+                        column: x => x.PostId,
+                        principalSchema: "socials",
+                        principalTable: "Posts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostImg_PostId",
+                schema: "socials",
+                table: "PostImg",
+                column: "PostId",
+                unique: true);
+        }
+    }
+}
